12ccd6a50d5afd9f6b20279eba84877539cc4ba5
shuki <shukivaknin@gmail.com>
2026-03-11 22:45:11 +0200

    Bump version to 0.21

--- web/app.py
+++ web/app.py
@@ -98,7 +98,7 @@ def create_app():
             "nav_items": nav_items,
             "active_page": active_page,
             "app_name": "GNIZA",
-            "app_version": "0.1.2",
+            "app_version": "0.21",
             "current_year": datetime.now().year,
         }
 
